f99d047fda64c2a1e31681cdff958891f1349a1a
Dan Phiffer <dan@phiffer.org>
2012-03-18 15:43:39 -0400

    Refactored the media archive

--- app/forum/show_archive.lua
+++ app/forum/show_archive.lua
@@ -6,19 +6,10 @@ show_header({
 })
 
 io.write('<div id="articles">')
-files = get_media(archive_base .. "/videos", "mp4")
+files = get_media_archive(archive_base)
 for i,file in ipairs(files) do
-  local title = string.sub(file, 14, -5)
-  local date = split(string.sub(file, 0, 10), '-')
-  local time = os.time({
-    year = tonumber(date[1]),
-    month = tonumber(date[2]),
-    day = tonumber(date[3])
-  })
-  local date_formatted = os.date('%b %d, %Y', time)
-  local file_encoded = url_encode(file)
-  file_encoded = string.gsub(file_encoded, '&', '&amp;')
-  print('<article><a href="?x=video&file=' .. file_encoded .. '"><span class="title">' .. title .. '</span><span class="date">' .. date_formatted .. "</span></a></article>")
+  local media = get_media_detail(file)
+  io.write(archive_html(media))
 end
 io.write('</div>')
 
