28fc2ac574cba31ea5e8211a136d3c96c1da239a
Dan Phiffer <dan@phiffer.org>
2012-07-19 16:51:17 -0700

    Removed unecessary files

--- app/views/forum/_message.php
+++
@@ -1,5 +0,0 @@
-<h3>
-  <?php echo get_username($message->user_id); ?>
-  <span class="when"><?php echo elapsed_time($message->created); ?></span>
-</h3>
-<?php echo nl2br(htmlentities($message->content)); ?>
--- app/views/media/_download_file.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$ext = '';
-if (preg_match('/\.([^.]+)$/', $file->path, $matches)) {
-  $ext = $matches[1];
-}
-echo "<a href=\"uploads/$file->path\" class=\"download\">download $ext</a>&nbsp;&nbsp;";
-
-?>
