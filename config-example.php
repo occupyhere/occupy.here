515f9da82e74e5490baf81e96276a3632094eee6
Dan Phiffer <dan@phiffer.org>
2012-08-26 17:07:30 -0400

    Added some comments to config

--- config-example.php
+++ config-example.php
@@ -1,7 +1,12 @@
 <?php
 
+// Hostname
+define('CANONICAL_HOST', 'occupy.here');
+
+// Your current timezone
 date_default_timezone_set('America/New_York');
-define('CANONICAL_HOST', 'occupy.dev');
+
+// Utilities
 define('SQLITE_CLI', '/usr/bin/sqlite3');
 define('ZIP', '/usr/bin/zip');
 
