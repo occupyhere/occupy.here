d19aa5d72cd86e0639065377330592427fb76bd5
Dan Phiffer <dan@phiffer.org>
2012-08-26 17:08:00 -0400

    Fixed a typo

--- init.php
+++ init.php
@@ -1,10 +1,19 @@
 <?php
 
-$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ||
+             $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 
 define('GRID_DIR', dirname(__FILE__));
 define('GRID_PATH', dirname($_SERVER['PHP_SELF']));
 define('GRID_URL', $protocol . $_SERVER['HTTP_HOST'] . GRID_PATH);
+
+if (!file_exists(GRID_DIR . '/config.php')) {
+  if (is_writable(GRID_DIR)) {
+    copy(GRID_DIR . '/config-example.php', GRID_DIR . '/config.php');
+  } else {
+    die("Please create config.php (see: config-example.php).");
+  }
+}
 
 require_once GRID_DIR . '/config.php';
 require_once GRID_DIR . '/library/autoload.php';
