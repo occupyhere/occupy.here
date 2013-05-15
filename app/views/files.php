1f27010ad144723644d439ff066073b9a39df59d
Dan Phiffer <dan@phiffer.org>
2013-05-15 13:11:35 -0400

    Tweaks to file index

--- app/views/files.php
+++ app/views/files.php
@@ -2,17 +2,18 @@
   <h2>All Files</h2>
   <?php if (empty($files)) { ?>
      <p class="pagination"><em>There is nothing here yet</em></p>
+  <?php } else { ?>
+    <ul>
+      <?php foreach ($files as $file) { ?>
+        <?php $topic = $topics[$file->message_id]; ?>
+        <li id="file_<?php echo $file->id; ?>" class="file user_<?php echo $file->user_id; ?>" data-colors="<?php echo get_colors($file->user_id); ?>">
+          <a href="<?php echo "p/$file->message_id"; ?>" class="filename"><?php echo $file->name; ?></a>
+          <div class="meta">
+            <span class="author"><a href="u/<?php echo $file->user_id; ?>" class="id"><span class="color"></span><?php echo get_username($file->user_id); ?></a></span>
+            <span class="when"><?php echo elapsed_time($file->created); ?></span>
+          </div>
+        </li>
+      <?php } ?>
+    </ul>
   <?php } ?>
-  <ul>
-    <?php foreach ($files as $file) { ?>
-      <?php $topic = $topics[$file->message_id]; ?>
-      <li id="file_<?php echo $file->id; ?>" class="file user_<?php echo $file->user_id; ?>" data-colors="<?php echo get_colors($file->user_id); ?>">
-        <a href="<?php echo "p/$file->message_id"; ?>" class="filename"><?php echo $file->name; ?></a>
-        <div class="meta">
-          <span class="author"><a href="u/<?php echo $file->user_id; ?>" class="id"><span class="color"></span><?php echo get_username($file->user_id); ?></a></span>
-          <span class="when"><?php echo elapsed_time($file->created); ?></span>
-        </div>
-      </li>
-    <?php } ?>
-  </ul>
 </div>
