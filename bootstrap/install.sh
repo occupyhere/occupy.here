861d39a8d26950d59686fd632dbd00524c63da20
Dan Phiffer <dan@phiffer.org>
2013-10-19 12:27:37 -0400

    New install script and firmware

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -7,6 +7,25 @@ echo "Set the root password (enables ssh access)"
 echo "------------------------------------------"
 passwd
 
+if ! df | grep " /usb" > /dev/null
+then
+  mount /dev/sda1 /usb
+fi
+
+if ! df | grep " /opt" > /dev/null
+then
+  if [ ! -e /usb/occupy.here/data/opt.img ]
+  then
+    echo
+    echo "Creating opt.img disk image"
+    echo "---------------------------"
+    echo "This will probably take a few minutes..."
+    dd if=/dev/zero of=/usb/occupy.here/data/opt.img bs=4096 count=32768
+    mkfs.ext4 -F /usb/occupy.here/data/opt.img
+  fi
+  mount /usb/occupy.here/data/opt.img /opt
+fi
+
 echo
 echo "Installing packages (1/5)"
 echo "-------------------------"
@@ -33,51 +52,68 @@ echo "-------------------------"
 opkg -dest opt install /usb/occupy.here/bootstrap/step5/*
 
 echo
-echo "Finishing up"
-echo "------------"
+echo "Configuring things"
+echo "------------------"
+
+symlink()
+{
+  if [ ! -h "$2" ]
+  then
+    ln -s "$1" "$2"
+  fi
+}
+
 echo "Setting up symlinks..."
-ln -s /opt/lib/libpthread-0.9.33.2.so /lib/libpthread.so.0
-ln -s /opt/lib/librt-0.9.33.2.so /lib/librt.so.0
-ln -s /opt/usr/lib/libcrypto.so.1.0.0 /usr/lib/libcrypto.so.1.0.0
-ln -s /opt/usr/lib/libncurses.so.5.7 /usr/lib/libcurses.so
-ln -s /opt/usr/lib/libform.so.5.7 /usr/lib/libform.so.5
-ln -s /opt/usr/lib/libform.so.5.7 /usr/lib/libform.so
-ln -s /opt/usr/lib/libjpeg.so.62.0.0 /usr/lib/libjpeg.so.62
-ln -s /opt/usr/lib/libjpeg.so.62.0.0 /usr/lib/libjpeg.so
-ln -s /opt/usr/lib/libmenu.so.5.7 /usr/lib/libmenu.so.5
-ln -s /opt/usr/lib/libmenu.so.5.7 /usr/lib/libmenu.so
-ln -s /opt/usr/lib/libncurses.so.5.7 /usr/lib/libncurses.so.5
-ln -s /opt/usr/lib/libncurses.so.5.7 /usr/lib/libncurses.so
-ln -s /opt/usr/lib/libpanel.so.5.7 /usr/lib/libpanel.so.5
-ln -s /opt/usr/lib/libpanel.so.5.7 /usr/lib/libpanel.so
-ln -s /opt/usr/lib/libpcre.so.0.0.1 /usr/lib/libpcre.so.0
-ln -s /opt/usr/lib/libpcreposix.so.0.0.0 /usr/lib/libpcreposix.so.0
-ln -s /opt/usr/lib/libreadline.so.5.2 /usr/lib/libreadline.so.5
-ln -s /opt/usr/lib/libreadline.so.5.2 /usr/lib/libreadline.so
-ln -s /opt/usr/lib/libsqlite3.so.0.8.6 /usr/lib/libsqlite3.so.0
-ln -s /opt/usr/lib/libssl.so.1.0.0 /usr/lib/libssl.so.1.0.0
-ln -s /opt/usr/lib/libxml2.so.2.7.8 /usr/lib/libxml2.so.2
-ln -s /opt/usr/lib/libz.so.1.2.7 /usr/lib/libz.so.1
-ln -s /opt/usr/lib/libz.so.1.2.7 /usr/lib/libz.so
-ln -s /opt/usr/lib/lighttpd /usr/lib/lighttpd
-ln -s /opt/usr/lib/opkg /usr/lib/opkg
-ln -s /opt/usr/lib/php /usr/lib/php
-ln -s /opt/usr/bin/php-cgi /usr/bin/php-fcgi
-ln -s /opt/usr/bin/php-cli /usr/bin/php
-ln -s /opt/usr/bin/sqlite3 /usr/bin/sqlite3
-ln -s /opt/usr/bin/jpegtran /usr/bin/jpegtran
-ln -s /opt/usr/sbin/lighttpd /usr/sbin/lighttpd
-ln -s /opt/usr/share/zoneinfo /usr/share/zoneinfo
-ln -s /opt/etc/lighttpd /etc/lighttpd
-ln -s /opt/etc/init.d/lighttpd /etc/init.d/lighttpd
-ln -s /opt/etc/init.d/lighttpd /etc/rc.d/S90lighttpd
-ln -s /opt/etc/init.d/php5-fastcgi /etc/init.d/php5-fastcgi
+
+symlink /opt/lib/libpthread-0.9.33.2.so /lib/libpthread.so.0              
+symlink /opt/lib/librt-0.9.33.2.so /lib/librt.so.0                        
+symlink /opt/usr/lib/libcrypto.so.1.0.0 /usr/lib/libcrypto.so.1.0.0       
+symlink /opt/usr/lib/libncurses.so.5.7 /usr/lib/libcurses.so              
+symlink /opt/usr/lib/libform.so.5.7 /usr/lib/libform.so.5                 
+symlink /opt/usr/lib/libform.so.5.7 /usr/lib/libform.so                   
+symlink /opt/usr/lib/libjpeg.so.62.0.0 /usr/lib/libjpeg.so.62             
+symlink /opt/usr/lib/libjpeg.so.62.0.0 /usr/lib/libjpeg.so                
+symlink /opt/usr/lib/libmenu.so.5.7 /usr/lib/libmenu.so.5                 
+symlink /opt/usr/lib/libmenu.so.5.7 /usr/lib/libmenu.so                   
+symlink /opt/usr/lib/libncurses.so.5.7 /usr/lib/libncurses.so.5      
+symlink /opt/usr/lib/libncurses.so.5.7 /usr/lib/libncurses.so        
+symlink /opt/usr/lib/libpanel.so.5.7 /usr/lib/libpanel.so.5          
+symlink /opt/usr/lib/libpanel.so.5.7 /usr/lib/libpanel.so            
+symlink /opt/usr/lib/libpcre.so.0.0.1 /usr/lib/libpcre.so.0          
+symlink /opt/usr/lib/libpcreposix.so.0.0.0 /usr/lib/libpcreposix.so.0
+symlink /opt/usr/lib/libreadline.so.5.2 /usr/lib/libreadline.so.5    
+symlink /opt/usr/lib/libreadline.so.5.2 /usr/lib/libreadline.so      
+symlink /opt/usr/lib/libsqlite3.so.0.8.6 /usr/lib/libsqlite3.so.0    
+symlink /opt/usr/lib/libssl.so.1.0.0 /usr/lib/libssl.so.1.0.0        
+symlink /opt/usr/lib/libxml2.so.2.7.8 /usr/lib/libxml2.so.2          
+symlink /opt/usr/lib/libz.so.1.2.7 /usr/lib/libz.so.1                
+symlink /opt/usr/lib/libz.so.1.2.7 /usr/lib/libz.so                  
+symlink /opt/usr/lib/lighttpd /usr/lib/lighttpd                      
+symlink /opt/usr/lib/opkg /usr/lib/opkg                              
+symlink /opt/usr/lib/php /usr/lib/php                                
+symlink /opt/usr/bin/php-cgi /usr/bin/php-fcgi                       
+symlink /opt/usr/bin/php-cli /usr/bin/php                            
+symlink /opt/usr/bin/sqlite3 /usr/bin/sqlite3                        
+symlink /opt/usr/bin/jpegtran /usr/bin/jpegtran                      
+symlink /opt/usr/sbin/lighttpd /usr/sbin/lighttpd                    
+symlink /opt/usr/share/zoneinfo /usr/share/zoneinfo                  
+symlink /opt/etc/lighttpd /etc/lighttpd                              
+symlink /opt/etc/init.d/lighttpd /etc/init.d/lighttpd                       
+symlink /opt/etc/init.d/lighttpd /etc/rc.d/S90lighttpd                      
+symlink /opt/etc/init.d/php5-fastcgi /etc/init.d/php5-fastcgi
 
 echo "Copying config files..."
+
+if [ -e /etc/lighttpd/lighttpd.conf ]
+then
+  mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.bak
+fi
+
 cp /usb/occupy.here/bootstrap/step6/config.php /usb/occupy.here/config.php
 cp /usb/occupy.here/bootstrap/step6/php.ini /etc/php.ini
 cp /usb/occupy.here/bootstrap/step6/lighttpd.conf /etc/lighttpd/lighttpd.conf
 cp /usb/occupy.here/bootstrap/step6/dnsmasq.conf /etc/dnsmasq.conf
+cp /usb/occupy.here/bootstrap/step6/banner /etc/config/banner
 
 echo
 echo "Generating GnuPG keys"
@@ -95,6 +131,9 @@ Expire-Date: 0
 EOF
 /opt/usr/bin/gpg --batch --gen-key /usb/occupy.here/data/gpg.conf
 
+echo
+echo "Finishing up"
+echo "------------"
 echo "Starting lighttpd..."
 /etc/init.d/lighttpd start
 echo "Restarting dnsmasq..."
@@ -107,9 +146,6 @@ echo "+-------------------------------------------------------------------------
 echo "|                                                                            |"
 echo "|  1. Join the wifi network 'OCCUPY.HERE / $MAC_ADDRESS'                     |"
 echo "|  2. Open a browser and go to the URL: http://occupy.here/                  |"
-echo "|                                                                            |"
-echo "|  Note: in order to login to ssh using public key encryption, copy your     |"
-echo "|        public key to /etc/config/dropbear/authorized_keys                  |"
 echo "|                                                                            |"
 echo "+----------------------------------------------------------------------------+"
 echo
