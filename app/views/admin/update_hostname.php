940bd42629ccf2ccd7da54b8cee6fbbaff080e0a
Dan Phiffer <dan@phiffer.org>
2013-11-24 17:33:27 -0500

    Validate hostname before updating

--- app/views/admin/update_hostname.php
+++ app/views/admin/update_hostname.php
@@ -2,8 +2,9 @@
 
 $hostname = HOSTNAME;
 
-if (!empty($_SESSION['is_admin']) && !empty($_POST['hostname'])) {
-  $hostname = $_POST['hostname'];
+if (!empty($_SESSION['is_admin']) && !empty($_POST['hostname']) &&
+    preg_match('/^[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*$/', $_POST['hostname'])) {
+  $hostname = strtolower($_POST['hostname']);
   $config = file_get_contents(GRID_DIR . '/config.php');
   $config = preg_replace("/(define\('HOSTNAME', )'([^']+)'/m", '$1' . "'$hostname'", $config);
   file_put_contents(GRID_DIR . '/config.php', $config);
